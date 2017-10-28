095f15b3cee9eb726097b951dfb9769e9564cffb
Sharma, Abhinav <abhsharm@visa.com>
2017-10-28 05:27:46 -0500

    pushing hw

--- PyPoll/main.py
+++ PyPoll/main.py
@@ -1,4 +1,5 @@
 file_path = 'election_data_2.csv'
+file_results = 'election_results.txt'
 import csv
 import collections
 """Declaring variables to hold the voting data"""
@@ -17,17 +18,26 @@ with open(file_path,'r') as csvfile:
 for row in raw_data:
        vote_dict[row[2]].append(row[0])
 
-
+file=open(file_results,'w')
 """Printing out the results"""
-print("Election Results")    
+file.write("Printing out the results\n")
+print("Election Results")   
+file.write("Election Results\n")   
 print("----------------------")
+file.write("----------------------\n")
 print("Total votes: "+"{:,}".format((len(raw_data))))
+file.write("Total votes: "+"{:,}".format((len(raw_data)))+"\n")
 print("----------------------")
+file.write("----------------------\n")
 for key,value in vote_dict.items():
-    print("No of votes for: "+str(key)+"("+str(len(value)*100/len(raw_data))+"%)"+" --> "+"{:,}".format(len(value)))
+    file.write("No of votes for: "+str(key)+"("+str(len(value)*100/len(raw_data))+"%)"+" --> "+"{:,}".format(len(value))+"\n")
     winner_dict[key] = len(value)
 for key, value in sorted(winner_dict.iteritems(), key=lambda (k,v): (v,k)):
-    print "%s: %s" % (key, value)
+    print("%s: %s" % (key, value))
+    file.write("%s: %s" % (key, value)+"\n")
     winners.append(key)
 print("-----------------------")
+file.write("-----------------------\n")
 print("Winner: "+str(winners[-1]))
+file.write("Winner: "+str(winners[-1])+"\n")
+file.close()
